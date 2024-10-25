2304061205935e378bb0bb26b6e667b66a400221
Lachovski <145782520+LachovskiJohnnie@users.noreply.github.com>
2024-10-25 08:39:50 -0300

    25/10/24

---
+++ Perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Fórmula 1',
+    'Qual foi o melhor Piloto de F1 Brasileiro?',
+    'Ayrton Senna.'
+)
+criaCartao(
+    'Filosofia/História',
+    'Qual foi o filósofo mais importante da história?',
+    'Heráclito de Éfeso (540-470 a.C.).'
+)
+criaCartao(
+    'História/Música',
+    'Qual a musica mais famosa de beethoven?',
+    'Nona Sinfonia.'
+)
+criaCartao(
+    'Futebol',
+    'Quem é o Rei do Futebol?',
+    'Pelé.'
+)
