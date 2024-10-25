413789ad7993de1dc0a7f2d5efcab6f1db4629fd
Lachovski <145782520+LachovskiJohnnie@users.noreply.github.com>
2024-10-25 08:55:40 -0300

    25/10/24 2.0

--- aap.js
+++ aap.js
@@ -1,3 +1,17 @@
 function criaCartao(categoria, pergunta, respota) {
-    console.log(categoria, pergunta, respota);
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+                    <h3>${categoria}</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>${pergunta}</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>O JavaScript é uma linguagem de programação</p>
+                    </div>
+    `
+    container.appendChild(cartao);
 }
